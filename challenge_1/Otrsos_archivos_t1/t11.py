fd871a8a598e1a0083ecdddf6d0b9cc0c4d9e475
rodrigoreact <ttdevelopreact@gmail.com>
2025-05-06 21:41:30 -0400

    Actualización

--- challenge_1/Otrsos_archivos_t1/t11.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Ruta con raw string para evitar errores con las barras
-archivo = r'C:\Users\User\Desktop\challenge_1\challenge_1\tienda_1.xlsx'
-
-# Leer las primeras 10 filas sin encabezados
-df = pd.read_excel(archivo, header=None)
-print(df.head(2360))
